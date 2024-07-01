9658c1184895cbf014f918eace95823c36f90cfa
Shubham Goyal <goyalshubham2521@gmail.com>
2024-07-02 01:37:54 +0530

    User creation added

---
+++ src/create-user.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      email: "goyalshubham@gmail.com",
+      name: "Shubham",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log("Done with the query");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
